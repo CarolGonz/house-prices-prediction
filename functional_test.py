61ecfd3cc0c4a8b2041c758f09e00f44c137eeb8
CarolGonz <carolgonzalezlujan@gmail.com>
2022-07-17 13:37:51 -0300

    Define scope

---
+++ functional_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class FunctionalTestCase(unittest.TestCase):
+
+    def test_main(self):
+        # # Should load the dataset from the sklearn api
+
+        # # Should do some analysis
+
+        # # Should return predictions
+
+        # # Should evaluate model's performance
+
+        self.fail("Still to finish the flow...")
+
+
+if __name__ == "__main__":
+    unittest.main()
